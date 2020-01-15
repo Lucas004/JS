bc3221e14eca5dd50fe5907cc2205fb46e20a612
Lucas004 <55288398+Lucas004@users.noreply.github.com>
2020-01-15 11:00:36 -0500

    Update myScripts.js

--- myScripts.js
+++ myScripts.js
@@ -3,35 +3,3 @@ for( i=0; i<31; i++) {
   document.write("Loop number" + i +"</br>")
 }
 var ans = Math.pow(2,i);
- 
-
-
-var i;
-for( i=150; i<201; i++) {
-  document.write("Loop number" + i +"</br>")
-}
-var ans = Math.pow(2,i);
-
-
-
-var i;
-for( i=0; i<101; i++) {
-  document.write("Loop number" + i +"</br>")
-}
-var ans = Math.pow(2,i);
-
-
-
-var i;
-for( i=100; i<501; i++) {
-  document.write("Loop number" + i +"</br>")
-}
-var ans = Math.pow(2,i);
-
-
-
-var i;
-for( i=0; i<257; i++) {
-  document.write("Loop number" + i +"</br>")
-}
-var ans = Math.pow(2,i);
